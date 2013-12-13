8f673fb0d49a101595ffb2cbf2aea6f9cd941fbb
Luke Smallman <mail@smllmn.com>
2013-12-13 09:10:08 +0000

    Added a simple isprime.py script for easy commandline primality testing.

---
+++ isprime.py
@@ -0,0 +1,9 @@
+import argparse
+from baillie_psw import baillie_psw
+
+parser = argparse.ArgumentParser(description='Check primality using Baillie-PSW')
+parser.add_argument('number', help='Return whether number is prime', type=int)
+args = parser.parse_args()
+
+if args.number:
+	print ('Prime' if baillie_psw(args.number) else 'Composite')
